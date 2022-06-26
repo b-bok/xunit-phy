55d1d475551fd6e8cf26d7e5bece5875cac5b47a
devbok0209 <devbok0209@bodycodi.com>
2022-06-27 00:10:18 +0900

    [CHPATER 20] testRunning 제거

--- xunit/TestCaseTest.py
+++ xunit/TestCaseTest.py
@@ -6,10 +6,6 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert self.test.wasRun
-
     def testSetUp(self):
         self.test.run()
-        assert self.test.wasSetUp
+        assert ("setUp " == self.test.log)
--- xunit/WasRun.py
+++ xunit/WasRun.py
@@ -9,6 +9,7 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + " testMethod"
 
     def run(self):
         method = getattr(self, self.name)
@@ -17,3 +18,5 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
+        self.log = "setUp "
+
